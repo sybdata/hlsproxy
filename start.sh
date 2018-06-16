fe8d0afa78a4d5cb5cf7732f33a544f550e65d4d
sybdata <as15@wt.de>
2018-06-16 17:37:18 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-/opt/acestream/start-engine --client-console --bind-all --access-token $1 --live-cache-type memory --allow_remote_access --state-dir /tmp/state/.ACEStream --cache-dir /tmp/state/.ACEStream --live-cache-size 524288000 --core-sandbox-max-peers 15 --live-buffer 25 --vod-buffer 10 --vod-drop-max-age 120 --stats-report-peers --port 9944 --service-remote-access --log-file /tmp/acestream.log >/dev/null 2>&1 &
+/opt/acestream/acestream.start >/dev/null 2>&1 &
 
 /opt/hlsp/hls-proxy >/dev/null 2>&1 &
 
 while true; do
-        sleep $2
-        rm -rf /tmp/state/.ACEStream/.acestream_cache/* /tmp/state/.ACEStream/collected_torrent_files/*
+        sleep $1
+        rm -rf /opt/acestream/androidfs/acestream.engine/.ACEStream/collected_torrent_files/*
 done
