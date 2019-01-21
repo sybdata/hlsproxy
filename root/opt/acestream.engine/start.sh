3b3804ffbd195be7cba3b85e987a774d750fbfc3
sybdata <as15@wt.de>
2019-01-21 14:49:24 +0100

    Update start.sh

--- root/opt/acestream.engine/start.sh
+++ root/opt/acestream.engine/start.sh
@@ -2,6 +2,8 @@
 
 python3 /opt/HTTPAceProxy-master/acehttp.py >/dev/null 2>&1 &
 
+/opt/hlsp/hls-proxy >/dev/null 2>&1 &
+
 while true; do
         sleep 600
         rm -rf /opt/acestream.engine/androidfs/acestream.engine/.ACEStream/collected_torrent_files/*
