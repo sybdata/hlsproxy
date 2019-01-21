ec160dc38b62e606566d3055c4a60e4a34834493
sybdata <as15@wt.de>
2019-01-21 14:48:49 +0100

    Create start.sh

---
+++ root/opt/acestream.engine/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+python3 /opt/HTTPAceProxy-master/acehttp.py >/dev/null 2>&1 &
+
+while true; do
+        sleep 600
+        rm -rf /opt/acestream.engine/androidfs/acestream.engine/.ACEStream/collected_torrent_files/*
+done
