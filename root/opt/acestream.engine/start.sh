6c3e322ee032313e3d01303cb4790e8c9f12efc2
sybdata <as15@wt.de>
2019-01-22 16:48:13 +0100

    Update start.sh

--- root/opt/acestream.engine/start.sh
+++ root/opt/acestream.engine/start.sh
@@ -2,7 +2,7 @@
 
 /opt/acestream.engine/start_acestream_chroot.sh >/dev/null 2>&1 &
 
-/opt/hlsp/hls-proxy >/dev/null 2>&1 &
+#/opt/hlsp/hls-proxy >/dev/null 2>&1 &
 
 while true; do
         sleep 600
